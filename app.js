f7031600628947cbc3bfdeff2d864dafef232391
levintoo <tooklevin@gmail.com>
2021-07-08 23:50:02 +0300

    prevent button from submitting

---
+++ app.js
@@ -0,0 +1,11 @@
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+todoButton.addEventListener('click', addTodo);
+
+function addTodo(event) {
+
+    // prevent form from submitting
+    event.preventDefault();
+}
