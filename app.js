31ec99481d9f488d8bdb73593431c556f7f73136
levintoo <tooklevin@gmail.com>
2021-07-09 10:36:08 +0300

    dynamic elemnts styling

--- app.js
+++ app.js
@@ -11,22 +11,25 @@ function addTodo(event) {
 // create div
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
+
 // create li
     const newTodo = document.createElement("li");
-    newTodo.innerHTML="First item";
+
+    newTodo.innerHTML= todoInput.value;
+    
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
 
     // create check mark button
     const completeButton = document.createElement('button');
     completeButton.innerHTML= '<i class="fas fa-check"></i>';
-    completeButton.classList.add("complete-tbn");
+    completeButton.classList.add("complete-btn");
     todoDiv.appendChild(completeButton);
 
      // create thrash mark button
      const thrashButton = document.createElement('button');
      thrashButton.innerHTML= '<i class="fas fa-trash"></i>';
-     thrashButton.classList.add("thrash-tbn");
+     thrashButton.classList.add("thrash-btn");
      todoDiv.appendChild(thrashButton);
 
      todoList.appendChild(todoDiv);
