08b4b8daf93e50a82fabf5235fc3789dcf1806d7
levintoo <tooklevin@gmail.com>
2021-07-11 19:48:46 +0300

    strike through completed

--- app.js
+++ app.js
@@ -45,4 +45,9 @@ function deleteCheck(e) {
         const todo = item.parentElement;
         todo.remove();
     }
+    //checkmark
+    if(item.classList[0] === "complete-btn"){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
 }
