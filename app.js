4343f2a2ce23a52497fa2455030c5b7a3c2f2460
levintoo <tooklevin@gmail.com>
2021-07-11 13:39:35 +0300

    creating elements

--- app.js
+++ app.js
@@ -4,5 +4,19 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
 // Event Listeners
+todoButton.addEventListener('click', addTodo);
 
 // functions
+function addTodo(event){
+    // prevents page from reloading
+    event.preventDefault();
+    // Todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    // create li
+    const newTodo = document.createElement("li");
+    newTodo.innerHTML= (todoInput.value);
+    newTodo.classList.add("todo-item");
+    newTodo.appendChild(newTodo);
+}
